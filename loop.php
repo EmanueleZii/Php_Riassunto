fb2fb2098b5d6e8cfea46a4e47f448730750386f
Emanuele Zignale <zignale92@gmail.com>
2025-07-02 14:58:57 +0200

    cicli

--- loop.php
+++ loop.php
@@ -12,6 +12,32 @@
 
 
 
-    
+    $i = 1;
+    do {
+        $i++;
+        echo "The number is " . $i . "<br>";
+    } 
+    while($i <= 3);
 
+    for ($i=1; $i<=3; $i++) {
+        echo "The number is " . $i . "<br>";
+    }   
+
+    $colors = array("Red", "Green", "Blue");
+
+    // Loop through colors array
+    foreach($colors as $value) {
+        echo $value . "<br>";
+    }
+
+    $superhero = array(
+    "name" => "Peter Parker",
+    "email" => "peterparker@mail.com",
+    "age" => 18
+    );
+    
+    // Loop through superhero array
+    foreach($superhero as $key => $value) {
+        echo $key . " : " . $value . "<br>";
+    } 
 ?>
